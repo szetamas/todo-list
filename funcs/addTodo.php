629b20ab04fe975da80a1e9ad8fbf82a771c25ac
Szedlák Tamás <tamas94@protonmail.com>
2023-11-15 16:48:09 +0100

    add response 200, changed some quote sign

--- funcs/addTodo.php
+++ funcs/addTodo.php
@@ -1,15 +1,15 @@
 <?php
-$inputDatas=file_get_contents("php://input");
+$inputDatas=file_get_contents('php://input');
 $datas=json_decode($inputDatas, true);
 
 //TODO: may here need <img> too, but it is MAY has a risk (xss)
-$enabledHtml="<div><b><strong><i><em><u><a><ul><ol><li><p><br><span>";
+$enabledHtml='<div><b><strong><i><em><u><a><ul><ol><li><p><br><span>';
 
-if( isset($datas["name"]) ) {
-  $name=strip_tags($datas["name"], $enabledHtml);
+if( isset($datas['name']) ) {
+  $name=strip_tags($datas['name'], $enabledHtml);
   
-  if( isset($datas["description"]) ) {
-    $description=strip_tags($datas["description"], $enabledHtml);
+  if( isset($datas['description']) ) {
+    $description=strip_tags($datas['description'], $enabledHtml);
   }
   if( strlen($name) > 0 && strlen($name) <= 80 ) {
 
@@ -28,8 +28,12 @@ if( isset($datas["name"]) ) {
       $stmt->bindParam(':description', $description);
 
       $stmt->execute();
+      http_response_code(200);
+      echo json_encode(['succes' => 'data is created']);
+      return;
     } catch(PDOException $error) {
       http_response_code(500);
+      //DONT SEND $error, may it could has some sensitive data
       echo json_encode(['error' => 'Something went wrong']);
       return;
     }
